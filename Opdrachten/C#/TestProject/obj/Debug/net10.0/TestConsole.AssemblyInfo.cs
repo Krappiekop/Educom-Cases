792a3a2c8b1e396836e46cf4622748bf2acc24b6
Bartijn M. Lap <bartijnmeurs@gmail.com>
2026-08-13 17:11:44 +0200

    PHP dir created. Lesson 1 completed

--- Opdrachten/C#/TestProject/obj/Debug/net10.0/TestConsole.AssemblyInfo.cs
+++ Opdrachten/C#/TestProject/obj/Debug/net10.0/TestConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1a9e70e7234dbe3bc3dbfd37b199d07d6deaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde44a3fb54baeea99c02c9012557c3579d85953")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Opdrachten/PHP/01/process_data2.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+    <?php
+
+    echo
+        "Hallo " . $_POST["name"] . "<br/>" .
+        "Je ingevulde email is " . $_POST["email"] . "<br/>" .
+        "Ingevulde telefoonnummer is " . $_POST["phone"] . "<br/>" .
+        "Opmerkingen: " . $_POST["remarks"];
+
+    ?>
+</body>
+
+</html>
